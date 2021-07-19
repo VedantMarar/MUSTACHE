22aca47cc94bf68e21d7ba78068edf7812de0004
VedantMarar <72292303+VedantMarar@users.noreply.github.com>
2021-07-19 17:28:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function take_snapshot(){
+    save('1.png');
+}
+
+function draw(){
+
+}
+
