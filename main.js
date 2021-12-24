2196bba4d0eddf21f2ba9cdbba2e4c4f7e76e126
VedantMarar <72292303+VedantMarar@users.noreply.github.com>
2021-12-24 09:08:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX = 0;
+noseY = 0;
+
 function preload(){
 
 }
@@ -5,6 +8,15 @@ function preload(){
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Posenet is initialized")
 }
 
 function take_snapshot(){
@@ -12,6 +24,16 @@ function take_snapshot(){
 }
 
 function draw(){
+image(video,0,0,300,300);
+}
 
+function gotPoses(){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X is"+ noseX);
+        console.log("Nose Y is", noseY);
+    }
 }
 
